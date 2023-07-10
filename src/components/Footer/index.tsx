443d24fc9f0a1a10f1110ce7cbe39af1441a2671
Ronniery Borges Correa <ronnisti@gmail.com>
2023-07-10 14:42:03 +0100

    chore: created initial version of Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,42 @@
+import { JSX } from "react/jsx-runtime";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import Grid from "@mui/material/Grid";
+
+const Footer = (): JSX.Element => {
+  return (
+    <Box>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Grid item>
+          <Button
+            variant="contained"
+            size="medium"
+            endIcon={<ChevronRightIcon />}
+          >
+            Create Randomly
+          </Button>
+        </Grid>
+        <Grid item xs={2} >
+          <Button
+            variant="contained"
+            size="medium"
+            endIcon={<ChevronRightIcon />}
+          >
+            Create New
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Footer;
