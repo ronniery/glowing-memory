82fce4f25e30f82cd9577fc6bf24a909c16530f2
Ronniery Borges Correa <ronnisti@gmail.com>
2023-07-10 14:43:21 +0100

    chore: remove not necessary code inside app and removed default css file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-
-import logo from "./logo.svg";
-import "./App.css";
+import TicketBoard from "./containers/TicketBoard";
+import Container from "@mui/material/Container";
 
 function App() {
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const { data } = await axios.get("http://localhost:46000/tickets");
-
-      console.log(`Fetched tickets: ${JSON.stringify(data)}`);
-    };
-
-    fetchTickets();
-  });
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Client Ticket Task!</p>
-        <p>
-          <b>Good Luck!</b>
-        </p>
-        <span className="App-span">
-          Make sure you read and understand the requirements in the README file
-        </span>
-      </header>
-    </div>
+    <Container>
+      <TicketBoard />
+    </Container>
   );
 }
 
