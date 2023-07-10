05ce115b4c8d5fd3378f08a49fb6145b44b2d4fd
Ronniery Borges Correa <ronnisti@gmail.com>
2023-07-10 15:16:20 +0100

    chore: created initial version of TicketList component

---
+++ src/components/TicketList/TicketList.styled.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+import styled from "styled-components";
+
+export const PageContainer = styled(Box)({
+  height: "calc(100% - 93px)",
+});
---
+++ src/components/TicketList/TicketList.tsx
@@ -0,0 +1,7 @@
+import { PageContainer } from "./TicketList.styled";
+
+const TicketList: React.FC = (): JSX.Element => {
+  return <PageContainer> Hey! </PageContainer>;
+};
+
+export default TicketList;
