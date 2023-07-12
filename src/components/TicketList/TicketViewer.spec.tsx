dfe6abb49b2e2d711afd2af4cda4b851a4a27a0f
Ronniery Borges Correa <ronnisti@gmail.com>
2023-07-12 15:47:07 +0100

    test: created test for TicketViewer component

---
+++ src/components/TicketList/TicketViewer.spec.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { useTickets } from '../../utils/contexts/ticket.context';
+import TicketViewer from './TicketViewer';
+import { Ticket } from '../../models/ticket.model';
+
+jest.mock('../../utils/contexts/ticket.context', () => ({
+  useTickets: jest.fn(),
+}));
+
+describe('TicketViewer', () => {
+  const position = 0;
+  const updateTicketByIdMock = jest.fn();
+  const ticket = {
+    _id: '123',
+    client: 'John Doe',
+    status: 'open',
+    issue: 'Sample issue',
+    deadline: new Date('2020/01/01'),
+    getFormattedDeadline: jest.fn(() => '2020/01/01'),
+    getOppositeStatus: jest.fn(),
+    getFlagStatus: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (useTickets as jest.Mock).mockReturnValue({
+      updateTicketById: updateTicketByIdMock,
+    });
+  });
+
+  it('renders the ticket viewer component correctly', () => {
+    render(<TicketViewer ticket={ticket as unknown as Ticket} position={position} />);
+
+    const ticketViewer = screen.getByTestId('ticket-viewer');
+    const ticketIndex = screen.getByTestId('ticket-index');
+    const ticketDate = screen.getByTestId('ticket-date');
+    const ticketSwitcher = screen.getByTestId('ticket-switcher');
+    const ticketStatus = screen.getByTestId('ticket-status');
+    const ticketMessage = screen.getByTestId('ticket-message');
+
+    expect(ticketViewer).toBeDefined();
+    expect(ticketIndex.textContent).toBe(`${position + 1}. ${ticket.client}`);
+    expect(ticketDate.textContent).toBe('2020/01/01');
+    expect(ticketSwitcher).toBeDefined();
+    expect(ticketStatus).toBeDefined();
+    expect(ticketMessage.getAttribute('placeholder')).toBe('Message');
+    expect(ticketMessage.getAttribute('unselectable')).toBe('on');
+    expect((ticketMessage as HTMLTextAreaElement).readOnly).toBeTruthy();
+    expect((ticketMessage as HTMLTextAreaElement).value).toBe('Sample issue');
+  });
+
+  it('calls updateTicketById with the correct parameters when the switch is toggled', () => {
+    render(<TicketViewer ticket={ticket as unknown as Ticket} position={position} />);
+
+    console.log(screen.debug());
+    const containerSwitcher = screen.getByTestId('ticket-switcher');
+    const ticketSwitcher = containerSwitcher.querySelector('input[type="checkbox"]');
+
+    fireEvent.click(ticketSwitcher as HTMLInputElement);
+
+    expect(updateTicketByIdMock).toHaveBeenCalledTimes(1);
+    expect(updateTicketByIdMock).toHaveBeenCalledWith(ticket._id, ticket.getOppositeStatus());
+  });
+});
--- src/components/TicketList/TicketViewer.tsx
+++ src/components/TicketList/TicketViewer.tsx
@@ -1,9 +1,8 @@
-import { useContext } from 'react';
 import { Grid, Typography, Switch } from '@mui/material';
 
 import { TicketClient, TicketStatus, TicketIssue, TicketContent } from './TicketViewer.styled';
+import { useTickets } from '../../utils/contexts/ticket.context';
 import { Ticket } from '../../models/ticket.model';
-import { TicketContext } from '../../utils/contexts/ticket.context';
 
 export type TicketViewerProps = {
   ticket: Ticket;
@@ -11,30 +10,37 @@ export type TicketViewerProps = {
 };
 
 const TicketViewer: React.FC<TicketViewerProps> = ({ ticket, position }): JSX.Element => {
-  const { updateTicketById } = useContext(TicketContext);
+  const { updateTicketById } = useTickets();
 
   return (
     <TicketContent data-testid="ticket-viewer" container alignItems="center" justifyContent="space-between">
       <Grid item zeroMinWidth xs={6}>
-        <TicketClient noWrap title={ticket.client}>
+        <TicketClient data-testid="ticket-index" noWrap title={ticket.client}>
           {position + 1}. {ticket.client}
         </TicketClient>
       </Grid>
       <Grid item xs={2}>
-        <Typography>{ticket.getFormattedDeadline()}</Typography>
+        <Typography data-testid="ticket-date">{ticket.getFormattedDeadline()}</Typography>
       </Grid>
       <Grid item>
         <Switch
+          data-testid="ticket-switcher"
           color="success"
           checked={ticket.status === 'open'}
           onChange={() => {
             updateTicketById(ticket._id as string, ticket.getOppositeStatus());
           }}
         />
-        <TicketStatus color={ticket.getFlagStatus()} checked />
+        <TicketStatus data-testid="ticket-status" color={ticket.getFlagStatus()} checked />
       </Grid>
       <Grid item xs={12}>
-        <TicketIssue placeholder="Message" readOnly unselectable="on" defaultValue={ticket.issue} />
+        <TicketIssue
+          data-testid="ticket-message"
+          placeholder="Message"
+          readOnly
+          unselectable="on"
+          defaultValue={ticket.issue}
+        />
       </Grid>
     </TicketContent>
   );
