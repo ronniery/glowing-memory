5faea48b6810abf38128da3361dd057d8d83a463
Ronniery Borges Correa <ronnisti@gmail.com>
2023-07-12 15:45:18 +0100

    test: created a small test for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test.todo('Render list of tasks', () => {});
+describe('App component', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+});
--- src/__mocks__/axios.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const mockAxios = jest.genMockFromModule<typeof axios>('axios');
-mockAxios.create = jest.fn(() => mockAxios);
-
-export default mockAxios;
